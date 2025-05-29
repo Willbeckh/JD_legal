fd339b6fd3638ae689d06edc22a203b5467e4908
Willbeckh <wileyhndirangu04@gmail.com>
2025-05-29 17:43:40 +0300

    Remove client role from user abstract model.
    - feature and functionality delegated to admin

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     ROLE_CHOICES = [
-        ('client', 'Client'),
         ('transcriber', 'Transcriber'),
         ('proofreader', "Proofreader"),
         ('admin', 'Admin')
