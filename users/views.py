30d880e1bf19807838e16aaf8f2d1b2d8263a9dd
Willbeckh <wileyhndirangu04@gmail.com>
2025-06-02 15:56:38 +0300

    Update user views.
    - add route to check is user is admin.
    - add route for admin to view all users.
    - add route to update user status and role.
    - create route to change role/status of a user.

--- users/views.py
+++ users/views.py
@@ -1,12 +1,11 @@
+from .models import User
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
-
-from .models import User
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer, UserListSerializer, UserUpdateSerializer
 
 class RegisterView(APIView):
     def post(self, request):
@@ -31,3 +30,35 @@ class MeView(APIView):
 
     def get(self, request):
         return Response(UserSerializer(request.user).data)
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.role == 'admin'
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+    lookup_field = 'id'
+    
+    
+class UserDeactivateView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+    lookup_field = 'id'
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
+        self.perform_destroy(user)
+        return Response({"detail": "User deactivated."}, status=status.HTTP_204_NO_CONTENT)
