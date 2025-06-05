e37fa42c402456880a94941dd95aca152020c6bb
Willbeckh <wileyhndirangu04@gmail.com>
2025-06-05 15:24:45 +0300

    Update Project model.
    - Add transcriber column filed: holds the id of assigned transcriber.
    - Add assigned proofreader id column.
    - create is_archived column to store activation status of a project.
    
    Update Transcriber model.
    
    - Include role choices on the roles column.
    - Create 'is_final' column for file history tracking.
    - Create custom method to save an uploaded transcript and allows
      'Admin'to mark as final version.

--- scriptapp/models.py
+++ scriptapp/models.py
@@ -1,22 +1,42 @@
 from django.db import models
 from users.models import User
 
+
 class Project(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     file_link = models.URLField()
-    admin = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'admin'})
+    admin = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={'role': 'admin'})
+    transcriber = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True, limit_choices_to={'role': 'transcriber'}, related_name="transcribed_projects")
+    proofreader = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True, limit_choices_to={'role': 'proofreader'}, related_name="proofread_projects")
     created_at = models.DateTimeField(auto_now_add=True)
+    is_archived = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
 
 class Transcript(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     file = models.FileField(upload_to='transcripts/')
     uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    role = models.CharField(max_length=20)  # 'transcriber' or 'proofreader'
+    role = models.CharField(max_length=20, choices=[('transcriber', 'Transcriber'), (
+        'proofreader', 'Proofreader')])  # 'transcriber' or 'proofreader'
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    is_final = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        """Ensures only one 'final' transcript per project-role pair."""
+        if self.is_final:
+            Transcript.objects.filter(
+                project=self.project,
+                role=self.role,
+                is_final=True
+            ).exclude(id=self.id).update(is_final=False)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.project.title} - {self.role}"
@@ -27,7 +47,7 @@ class Assignment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20)
 
-    class Meta: 
+    class Meta:
         unique_together = ('project', 'user', 'role')
 
     def __str__(self):
