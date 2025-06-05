7dcdb04bd1e9a441c3d23c0c991747365a31081a
Willbeckh <wileyhndirangu04@gmail.com>
2025-06-05 19:29:32 +0300

    Crreate logout view. Destroys user token on logout.

--- users/views.py
+++ users/views.py
@@ -3,34 +3,58 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics, permissions
-from rest_framework.authtoken.models import Token
 from .permissions import IsAdmin
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer, UserListSerializer, UserUpdateSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import (
+    RegisterSerializer,
+    UserSerializer,
+    UserListSerializer,
+    UserUpdateSerializer,
+    CustomTokenObtainPairSerializer,
+)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token = Token.objects.create(user=user)
-            return Response({'token': token.key, 'user': UserSerializer(user).data}, status=201)
+            # token = Token.objects.create(user=user)
+            return Response({"user": UserSerializer(user).data}, status=201)
         return Response(serializer.errors, status=400)
 
-class LoginView(APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key, 'user': UserSerializer(user).data})
-        return Response(serializer.errors, status=400)
 
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(UserSerializer(request.user).data)
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(
+                {"detail": "Successfully logged out."},
+                status=status.HTTP_205_RESET_CONTENT,
+            )
+        except Exception as e:
+            return Response(
+                {"error": "Invalid token or token already blacklisted."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
 class UserListView(generics.ListAPIView):
@@ -43,13 +67,13 @@ class UserUpdateView(generics.UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserUpdateSerializer
     permission_classes = [permissions.IsAuthenticated, IsAdmin]
-    lookup_field = 'id'
-    
-    
+    lookup_field = "id"
+
+
 class UserDeleteView(generics.DestroyAPIView):
     queryset = User.objects.all()
     permission_classes = [permissions.IsAuthenticated, IsAdmin]
-    lookup_field = 'id'
+    lookup_field = "id"
 
     def perform_destroy(self, instance):
         instance.is_active = False
@@ -57,7 +81,6 @@ class UserDeleteView(generics.DestroyAPIView):
 
     def delete(self, request, *args, **kwargs):
         user = self.get_object()
-            
-            
+
         self.perform_destroy(user)
         return Response({"detail": "User deleted."}, status=status.HTTP_204_NO_CONTENT)
