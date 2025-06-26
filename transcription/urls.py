9e83cae4ebc8be778fa56b85215c1cfa8d52ee2a
Willbeckh <wileyhndirangu04@gmail.com>
2025-06-26 17:08:25 +0300

    update path for static files

--- transcription/urls.py
+++ transcription/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from .views import welcome_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", welcome_view, name="root"),
     path("api/auth/", include("users.urls")),
     path("api/projects/", include("scriptapp.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
